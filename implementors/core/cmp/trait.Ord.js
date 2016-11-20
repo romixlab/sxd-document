d693b176c8216205a2585e845d4e8945ab8de048
Travis-CI <builder@travis>
2016-11-20 03:07:22 +0000

    Deployed to Github Pages

---
+++ implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["peresil"] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='peresil/struct.StringPoint.html' title='peresil::StringPoint'>StringPoint</a>&lt;'a&gt;",];implementors["sxd_document"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='enum' href='sxd_document/parser/enum.Error.html' title='sxd_document::parser::Error'>Error</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='sxd_document/struct.PrefixedName.html' title='sxd_document::PrefixedName'>PrefixedName</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["sxd_document"] = ["impl&lt;'d&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='sxd_document/dom/struct.Root.html' title='sxd_document::dom::Root'>Root</a>&lt;'d&gt;","impl&lt;'d&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='sxd_document/dom/struct.Element.html' title='sxd_document::dom::Element'>Element</a>&lt;'d&gt;","impl&lt;'d&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='sxd_document/dom/struct.Attribute.html' title='sxd_document::dom::Attribute'>Attribute</a>&lt;'d&gt;","impl&lt;'d&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='sxd_document/dom/struct.Text.html' title='sxd_document::dom::Text'>Text</a>&lt;'d&gt;","impl&lt;'d&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='sxd_document/dom/struct.Comment.html' title='sxd_document::dom::Comment'>Comment</a>&lt;'d&gt;","impl&lt;'d&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='sxd_document/dom/struct.ProcessingInstruction.html' title='sxd_document::dom::ProcessingInstruction'>ProcessingInstruction</a>&lt;'d&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
